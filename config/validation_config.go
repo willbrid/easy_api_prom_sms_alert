ca4b387e2dceae165f34c44b18d35153003c4aa4
willbrid <ngaswilly77@gmail.com>
2024-05-18 21:50:17 +0100

    feat: remove log

--- config/validation_config.go
+++ config/validation_config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"easy-api-prom-alert-sms/logging"
 	"fmt"
 
 	"github.com/go-playground/validator/v10"
@@ -16,29 +15,33 @@ func validateAuthConfig(v *viper.Viper, validate *validator.Validate) error {
 
 	if authEnabled {
 		if err := validate.Var(authUsername, "required,min=2,max=25"); err != nil {
-			logging.Log(logging.Error, "failed to validate auth.username field")
 			return fmt.Errorf("the field auth.username is required and must be a string between 2 and 25 characters long")
 		}
 
 		if err := validate.Var(authPassword, "required,min=8,max=255"); err != nil {
-			logging.Log(logging.Error, "failed to validate auth.password")
 			return fmt.Errorf("the field auth.password is required and must be a string between 8 and 255 characters long")
 		}
 
-		logging.Log(logging.Info, "auth config validation passed successfully")
 		return nil
 	}
 
 	v.Set("easy_api_prom_sms_alert.auth.username", "")
 	v.Set("easy_api_prom_sms_alert.auth.password", "")
 
-	logging.Log(logging.Info, "auth config validation passed successfully")
+	return nil
+}
+
+func validateProviderConfig(v *viper.Viper, validate *validator.Validate) error {
 	return nil
 }
 
 func validateConfig(v *viper.Viper, validate *validator.Validate) error {
 	// Validate authentication config
 	if err := validateAuthConfig(v, validate); err != nil {
+		return err
+	}
+
+	if err := validateProviderConfig(v, validate); err != nil {
 		return err
 	}
 
