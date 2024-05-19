11e828e65a74c8d53653810a14c0ca668a4a3be4
willbrid <ngaswilly77@gmail.com>
2024-05-19 08:18:36 +0100

    feat: add recipients validation configuration

--- config/validation_config.go
+++ config/validation_config.go
@@ -101,6 +101,53 @@ func validateProviderConfig(v *viper.Viper, validate *validator.Validate) error 
 	return nil
 }
 
+func validateRecipientsConfig(v *viper.Viper, validate *validator.Validate) error {
+	recipients := v.Get("easy_api_prom_sms_alert.recipients")
+	recipientList, ok := recipients.([]interface{})
+	if !ok {
+		return fmt.Errorf("error converting recipients to slice of interface{}")
+	}
+
+	if len(recipientList) > 50 {
+		return fmt.Errorf("recipients configuration must contain at most 50 items")
+	}
+
+	for _, recipient := range recipientList {
+		recipientMap, ok := recipient.(map[string]interface{})
+		if !ok {
+			return fmt.Errorf("error converting recipient to map[string]interface{}")
+		}
+
+		if err := validate.Var(recipientMap["name"], "required,max=25"); err != nil {
+			return fmt.Errorf("the field recipients[].name is required and must be a string at most 25 characters long")
+		}
+
+		members, ok := recipientMap["members"].([]interface{})
+		if !ok {
+			return fmt.Errorf("error converting recipientMap['members'] to slice of interface{}")
+		}
+		if len(members) > 50 {
+			return fmt.Errorf("recipients members configuration must contain at most 50 items")
+		}
+
+		for _, member := range members {
+			memberMap, ok := member.(map[string]interface{})
+			if !ok {
+				return fmt.Errorf("error converting member to map[string]interface{}")
+			}
+
+			if err := validate.Var(memberMap["name"], "required,max=25"); err != nil {
+				return fmt.Errorf("the field recipients[].members[].name is required and must be a string at most 25 characters long")
+			}
+			if err := validate.Var(memberMap["phone_number"], "required,max=25"); err != nil {
+				return fmt.Errorf("the field recipients[].members[].phone_number is required and must be a string at most 25 characters long")
+			}
+		}
+	}
+
+	return nil
+}
+
 // validateConfig validate the entire configuration
 func validateConfig(v *viper.Viper, validate *validator.Validate) error {
 	// Validate authentication config
@@ -109,6 +156,10 @@ func validateConfig(v *viper.Viper, validate *validator.Validate) error {
 	}
 
 	if err := validateProviderConfig(v, validate); err != nil {
+		return err
+	}
+
+	if err := validateRecipientsConfig(v, validate); err != nil {
 		return err
 	}
 
