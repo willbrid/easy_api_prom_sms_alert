183c64cdcd17b918fb2675bbcf82986984eee7de
willbrid <ngaswilly77@gmail.com>
2024-05-20 16:07:00 +0100

    feat: define http server

--- main.go
+++ main.go
@@ -5,24 +5,43 @@ import (
 	"easy-api-prom-alert-sms/logging"
 
 	"flag"
+	"net/http"
+	"strconv"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
 )
 
 var validate *validator.Validate
 
 func main() {
-	// Load configuration file
-	var configFile string
-	flag.StringVar(&configFile, "config-file", "config.default.yaml", "Chemin du fichier de configuration")
-	flag.Parse()
+	var (
+		configFile string
+		listenPort int
+	)
 
+	flag.StringVar(&configFile, "config-file", "config.default.yaml", "Chemin du fichier de configuration")
+	flag.IntVar(&listenPort, "port", 5957, "port d'écoute")
+	flag.Parse()
 	validate = validator.New(validator.WithRequiredStructEnabled())
+
+	if err := validate.Var(listenPort, "required,min=1024,max=49151"); err != nil {
+		logging.Log(logging.Error, "you should provide a port number between 1024 and 49151")
+		return
+	}
+
 	_, err := config.LoadConfig(configFile, validate)
 	if err != nil {
 		logging.Log(logging.Error, "error loading configuration")
 		return
 	}
 
-	// The logic of sms alert
+	router := mux.NewRouter()
+	router.HandleFunc("/api-alert", alertHandler).Methods("POST")
+	logging.Log(logging.Info, "Server is listening on port %v", strconv.Itoa(listenPort))
+	err = http.ListenAndServe(":"+strconv.Itoa(listenPort), router)
+	if err != nil {
+		logging.Log(logging.Error, "Failed to start server: %v", err.Error())
+		return
+	}
 }
