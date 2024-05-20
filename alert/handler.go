8b65bae1131d2ca0e64d86a0a0a347627ff8b9dc
willbrid <ngaswilly77@gmail.com>
2024-05-20 18:31:28 +0100

    feat: finalize AlertHandler implementation

--- alert/handler.go
+++ alert/handler.go
@@ -1,10 +1,37 @@
 package alert
 
-import "net/http"
+import (
+	"easy-api-prom-alert-sms/logging"
+	"encoding/json"
+	"net/http"
+
+	"github.com/prometheus/alertmanager/template"
+)
 
 func (alertSender *AlertSender) AlertHandler(resp http.ResponseWriter, req *http.Request) {
-	// Définir la logique d'envoie de SMS
+	var alertData template.Data
+	err := json.NewDecoder(req.Body).Decode(&alertData)
+	if err != nil {
+		logging.Log(logging.Error, "failed to parse content : %s", err.Error())
+		http.Error(resp, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	alertSender.setData(&alertData)
+
+	err = alertSender.sendAlert()
+	if err != nil {
+		logging.Log(logging.Error, "failed to send alert : %s", err.Error())
+		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	resp.Header().Set("Content-Type", "application/json")
 	resp.WriteHeader(http.StatusNoContent)
+}
+
+func (alertSender *AlertSender) sendAlert() error {
+	// Définir la logique d'envoie de SMS
+
+	return nil
 }
