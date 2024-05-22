449690d3d2f423ffffd2746caf5b1e682532c5ef
willbrid <ngaswilly77@gmail.com>
2024-05-22 22:49:44 +0100

    feat: update validation config

--- config/validation_config.go
+++ config/validation_config.go
@@ -41,31 +41,14 @@ func validateProviderConfig(v *viper.Viper, validate *validator.Validate) error 
 
 	// validate provider authentication config
 	providerAuthEnabled := v.GetBool("easy_api_prom_sms_alert.provider.authentication.enabled")
-	providerAuthBasicUsername := v.GetString("easy_api_prom_sms_alert.provider.authentication.basic.username")
-	providerAuthBasicPassword := v.GetString("easy_api_prom_sms_alert.provider.authentication.basic.password")
 	providerAuthAuthorizationType := v.GetString("easy_api_prom_sms_alert.provider.authentication.authorization.type")
 	providerAuthAuthorizationCredential := v.GetString("easy_api_prom_sms_alert.provider.authentication.authorization.credential")
 	if providerAuthEnabled {
-		if (len(providerAuthBasicUsername) == 0 && len(providerAuthAuthorizationType) == 0) || (len(providerAuthBasicUsername) != 0 && len(providerAuthAuthorizationType) != 0) {
-			return fmt.Errorf("when provider.authentication is enabled, you should provider either basic or authorization config but not both")
-		}
-
-		if len(providerAuthBasicUsername) != 0 {
-			v.Set("easy_api_prom_sms_alert.provider.authentication.authorization.type", "")
-			v.Set("easy_api_prom_sms_alert.provider.authentication.authorization.credential", "")
-
-			if err := validate.Var(providerAuthBasicUsername, "required,max=255"); err != nil {
-				return fmt.Errorf("when provider.authentication.basic is used, the field provider.authentication.basic.username is required and must be a string at most 255 characters long")
-			}
-			if err := validate.Var(providerAuthBasicPassword, "required,max=255"); err != nil {
-				return fmt.Errorf("when provider.authentication.basic is used, the field provider.authentication.basic.password is required and must be a string at most 255 characters long")
-			}
+		if len(providerAuthAuthorizationType) == 0 {
+			return fmt.Errorf("when provider.authentication is enabled, you should provider authorization config")
 		}
 
 		if len(providerAuthAuthorizationType) != 0 {
-			v.Set("easy_api_prom_sms_alert.provider.authentication.basic.username", "")
-			v.Set("easy_api_prom_sms_alert.provider.authentication.basic.password", "")
-
 			if err := validate.Var(providerAuthAuthorizationType, "required,oneof='Bearer Basic ApiKey'"); err != nil {
 				return fmt.Errorf("when provider.authentication.authorization is used, the field provider.authentication.authorization.type must be among the values : Bearer, Basic, ApiKey")
 			}
@@ -74,8 +57,6 @@ func validateProviderConfig(v *viper.Viper, validate *validator.Validate) error 
 			}
 		}
 	} else {
-		v.Set("easy_api_prom_sms_alert.provider.authentication.basic.username", "")
-		v.Set("easy_api_prom_sms_alert.provider.authentication.basic.password", "")
 		v.Set("easy_api_prom_sms_alert.provider.authentication.authorization.type", "")
 		v.Set("easy_api_prom_sms_alert.provider.authentication.authorization.credential", "")
 	}
