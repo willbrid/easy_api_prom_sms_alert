14d137bbb695c928668e0aea5a78def385ec25ba
willbrid <ngaswilly77@gmail.com>
2024-05-31 23:27:25 +0100

    refactor: add sms util

---
+++ utils/sms.go
@@ -0,0 +1,54 @@
+package utils
+
+import (
+	"easy-api-prom-alert-sms/logging"
+
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
+
+// SendSMSFromApi send sms through an api specification
+func SendSMSFromApi(url string, body string, authEnable bool, authType string, authCred string, timeout time.Duration, simulation bool) error {
+	if simulation {
+		logging.Log(logging.Info, "successful send request with url %s and body %s", url, body)
+	} else {
+		client := &http.Client{
+			Timeout: timeout,
+		}
+
+		req, err := http.NewRequest("POST", url, strings.NewReader(body))
+		if err != nil {
+			return err
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+		if authEnable {
+			req.Header.Set("Authorization", authType+" "+authCred)
+		}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+
+		defer resp.Body.Close()
+
+		var respBody []byte
+		respBody, err = io.ReadAll(resp.Body)
+		if err != nil {
+			logging.Log(logging.Error, "Failed to read response body: %v", err)
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("request failed with status : %s", resp.Status)
+		}
+
+		logging.Log(logging.Info, "successful send request with url %s and body %s", url, body)
+		logging.Log(logging.Info, "response body %s", string(respBody))
+	}
+
+	return nil
+}
