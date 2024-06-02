af82fead34fce3d7201ce620fa824a2871d2e9ea
willbrid <ngaswilly77@gmail.com>
2024-06-02 15:20:00 +0100

    feat: add create temp config file for testing

---
+++ utils/file_utils.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+)
+
+func CreateConfigFileForTesting(configContent string) (string, error) {
+	configFile, err := os.CreateTemp("", "testconfig-*.yaml")
+	if err != nil {
+		return "", fmt.Errorf("unable to create temp file : %s", err.Error())
+	}
+
+	_, err = configFile.WriteString(configContent)
+	if err != nil {
+		return "", fmt.Errorf("unable to write to temp file : %s", err.Error())
+	}
+	configFile.Close()
+
+	return configFile.Name(), nil
+}
