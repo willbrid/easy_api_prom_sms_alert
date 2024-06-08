e800f8ec03be555523549e4f5a7d6b712f479c35
willbrid <ngaswilly77@gmail.com>
2024-06-08 10:03:04 +0100

    refactor: update validateAuthConfig func

--- config/config_auth.go
+++ config/config_auth.go
@@ -9,23 +9,43 @@ import (
 
 type Auth struct {
 	Enabled  bool   `mapstructure:"enabled"`
-	Username string `mapstructure:"username"`
-	Password string `mapstructure:"password"`
+	Username string `mapstructure:"username" validate:"required_with=Enabled,min=2,max=25"`
+	Password string `mapstructure:"password" validate:"required_with=Enabled,min=8"`
 }
+
+const (
+	errParsedAuth   string = "unable to parse easy_api_prom_sms_alert.auth"
+	errAuthUsername string = "the field auth.username is required and must be a string between 2 and 25 characters long"
+	errAuthPassword string = "the field auth.password is required and must must have at least 8 characters long"
+)
 
 // validateAuthConfig validate authentication configuration
 func validateAuthConfig(v *viper.Viper, validate *validator.Validate) error {
-	authEnabled := v.GetBool("easy_api_prom_sms_alert.auth.enabled")
-	authUsername := v.GetString("easy_api_prom_sms_alert.auth.username")
-	authPassword := v.GetString("easy_api_prom_sms_alert.auth.password")
+	authField := "easy_api_prom_sms_alert.auth"
+	var auth Auth
 
-	if authEnabled {
-		if err := validate.Var(authUsername, "required,min=2,max=25"); err != nil {
-			return fmt.Errorf("the field auth.username is required and must be a string between 2 and 25 characters long")
-		}
+	if err := v.Sub(authField).Unmarshal(&auth); err != nil {
+		return fmt.Errorf(errParsedAuth+": %w", err)
+	}
 
-		if err := validate.Var(authPassword, "required,min=8,max=255"); err != nil {
-			return fmt.Errorf("the field auth.password is required and must be a string between 8 and 255 characters long")
+	if auth.Enabled {
+		err := validate.Struct(auth)
+
+		if err != nil {
+			if _, ok := err.(*validator.InvalidValidationError); ok {
+				return err
+			}
+
+			for _, err := range err.(validator.ValidationErrors) {
+				switch err.Field() {
+				case "Username":
+					return fmt.Errorf(errAuthUsername)
+				case "Password":
+					return fmt.Errorf(errAuthPassword)
+				default:
+					return fmt.Errorf("validation failed for " + authField)
+				}
+			}
 		}
 
 		return nil
