08d2f5b4963fbe4d5fdda40aeeb0432190275854
willbrid <ngaswilly77@gmail.com>
2024-07-08 00:30:39 +0100

    fix: add default param_value for message param

--- config/config.go
+++ config/config.go
@@ -71,7 +71,7 @@ func setConfigDefaults(v *viper.Viper) {
 	v.SetDefault("easy_api_prom_sms_alert.provider.parameters.to.param_value", "")
 	v.SetDefault("easy_api_prom_sms_alert.provider.parameters.to.param_method", "")
 	v.SetDefault("easy_api_prom_sms_alert.provider.parameters.message.param_name", "")
-	v.SetDefault("easy_api_prom_sms_alert.provider.parameters.message.param_value", "")
+	v.SetDefault("easy_api_prom_sms_alert.provider.parameters.message.param_value", "<%message%>") // The templating feature will be implement later
 	v.Set("easy_api_prom_sms_alert.provider.parameters.message.param_method", utils.PostMethod)
 	v.SetDefault("easy_api_prom_sms_alert.provider.timeout", "10s")
 	v.SetDefault("easy_api_prom_sms_alert.recipients", make([]Recipient, 0))
