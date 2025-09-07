f262bf886be2f78c515174b09b4586c815b0b8e1
willbrid <ngaswilly77@gmail.com>
2025-09-07 18:16:11 +0200

    refactor: integrate new logger package

--- cmd/main.go
+++ cmd/main.go
@@ -3,32 +3,31 @@ package main
 import (
 	"easy-api-prom-alert-sms/config"
 	"easy-api-prom-alert-sms/internal/app"
-	"easy-api-prom-alert-sms/logging"
+	"easy-api-prom-alert-sms/pkg/logger"
 
 	"github.com/go-playground/validator/v10"
 )
 
 func main() {
 	var validate *validator.Validate = validator.New(validator.WithRequiredStructEnabled())
+	l := logger.NewLogger()
 
 	configFlag, err := config.LoadConfigFlag(validate)
 	if err != nil {
-		logging.Log(logging.Error, "%s", err.Error())
-		return
+		l.Fatal("failed to load configuration flags: %v", err.Error())
 	}
 
 	viperInstance, err := config.ReadConfigFile(configFlag.ConfigFile)
 	if err != nil {
-		logging.Log(logging.Error, "%s", err.Error())
-		return
+		l.Fatal("failed to read configuration file: %v", err.Error())
 	}
 
 	configLoaded, err := config.LoadConfig(viperInstance, validate)
 	if err != nil {
-		logging.Log(logging.Error, "%s", err.Error())
-		return
+		l.Fatal("failed to load configuration file: %v", err.Error())
 	}
-	logging.Log(logging.Info, "configuration file '%s' was loaded successfully", configFlag.ConfigFile)
 
-	app.Run(configLoaded, configFlag)
+	l.Info("configuration file '%s' was loaded successfully", configFlag.ConfigFile)
+
+	app.Run(configLoaded, configFlag, l)
 }
