5cb7795fb17cf61efd17c754e65a6d9585c7a019
willbrid <ngaswilly77@gmail.com>
2025-09-10 02:10:05 +0200

    feat; implement handlers for healthcheck and send alert

---
+++ internal/handler/handler.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"easy-api-prom-alert-sms/config"
+	"easy-api-prom-alert-sms/internal/entity"
+	"easy-api-prom-alert-sms/internal/usecase"
+	"easy-api-prom-alert-sms/pkg/logger"
+
+	"encoding/json"
+	"net/http"
+
+	"github.com/prometheus/alertmanager/template"
+)
+
+type Handler struct {
+	cfg     *config.Config
+	iAlert  usecase.IAlert
+	iLogger logger.ILogger
+}
+
+func newHandler(c *config.Config, a usecase.IAlert, l logger.ILogger) *Handler {
+	return &Handler{c, a, l}
+}
+
+func (c *Handler) HandleHealthCheck(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-Type", "application/json")
+	resp.WriteHeader(http.StatusNoContent)
+}
+
+func (c *Handler) HandleAlert(resp http.ResponseWriter, req *http.Request) {
+	var alertData template.Data
+
+	if err := json.NewDecoder(req.Body).Decode(&alertData); err != nil {
+		c.iLogger.Error("failed to parse content : %s", err.Error())
+		http.Error(resp, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	go func() {
+		if err := c.iAlert.Send(entity.Alert{Data: &alertData}, c.cfg); err != nil {
+			c.iLogger.Error("failed to send alert : %s", err.Error())
+		}
+	}()
+
+	resp.Header().Set("Content-Type", "application/json")
+	resp.WriteHeader(http.StatusNoContent)
+}
